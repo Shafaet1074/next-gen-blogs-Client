80a4a7e2be26ab0818302ea4c441f6ba43528429
shafaet.hossain@g.bracu.ac.bd <shafaet.hossain@g.bracu.ac.bd>
2024-05-15 23:34:15 +0600

    environment hidden

--- src/Firbease/Firebase.config.js
+++ src/Firbease/Firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBN4znlPnXvVvPrQTx_A6iHdmp3wq3JLls",
-  authDomain: "nextgenblogs-6e55f.firebaseapp.com",
-  projectId: "nextgenblogs-6e55f",
-  storageBucket: "nextgenblogs-6e55f.appspot.com",
-  messagingSenderId: "310674543724",
-  appId: "1:310674543724:web:6a9213d945325f9a75b449"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN ,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_STORAGEBUCKET,
+  appId:import.meta.env.VITE_APPID  
 };
 
 // Initialize Firebase
